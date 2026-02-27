b16a0c6879fa4394cdeb9e6731fe1433190300e5
manishimwekwizeraj <manishimwekwizeraj@gmail.com>
2026-02-27 19:26:45 +0200

    subclass PayPall extending superclass PaymentMethod

--- src/oop/polymorphism/paymentmethods/PayPal.java
+++ src/oop/polymorphism/paymentmethods/PayPal.java
@@ -1,4 +1,25 @@
 package oop.polymorphism.paymentmethods;
 
-public class PayPal {
+
+public class PayPal extends PaymentMethod{
+
+
+    @Override
+    public String processPayment(double amount) {
+        while(amount <0 ){
+
+            return "Can not process a negative amount!";
+        }
+        return "Card processed with PayPal: "+"card Owner: "+getCardOwner();
+
+    }
+
+    public PayPal(String owner, String cardNumber, double amount){
+        super(cardNumber, owner, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Owner name: "+getCardOwner()+" || "+"Card Number: "+getCardId() + " || " + "Amount: "+getAmount();
+    }
 }
