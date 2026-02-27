d1d85a540e0443c0c7dd0008bd360723f93ac24b
manishimwekwizeraj <manishimwekwizeraj@gmail.com>
2026-02-27 21:09:23 +0200

    fix: correct copy-paste ghost string to display Momo instead of PayPal

--- src/oop/polymorphism/paymentmethods/MobileMoney.java
+++ src/oop/polymorphism/paymentmethods/MobileMoney.java
@@ -3,11 +3,8 @@ package oop.polymorphism.paymentmethods;
 public class MobileMoney extends PaymentMethod{
     @Override
     public String processPayment(double amount) {
-        while(amount <0 ){
 
-            return "Can not process a negative amount!";
-        }
-        return "Card processed with PayPal: "+"card Owner: "+getCardOwner();
+        return "Card processed with Momo: "+"card Owner: "+getCardOwner();
 
     }
 
@@ -16,6 +13,6 @@ public class MobileMoney extends PaymentMethod{
     }
     @Override
     public String toString() {
-        return "Owner name: "+getCardOwner()+" || "+"Card Number: "+getCardId() + " || " + "Amount: "+getAmount();
+        return "Owner name: "+getCardOwner()+" || "+"Card Number: "+getCardId() + " || " + "Type: Mobile Money" + " || " + "Amount: "+getAmount();
     }
 }
