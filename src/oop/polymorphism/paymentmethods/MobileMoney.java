fc085b88f41028b700716616f52c55904d821a58
manishimwekwizeraj <manishimwekwizeraj@gmail.com>
2026-02-27 19:23:17 +0200

    subclass MobileMoney extending superclass PaymentMethod

--- src/oop/polymorphism/paymentmethods/MobileMoney.java
+++ src/oop/polymorphism/paymentmethods/MobileMoney.java
@@ -1,4 +1,21 @@
 package oop.polymorphism.paymentmethods;
 
-public class MobileMoney {
+public class MobileMoney extends PaymentMethod{
+    @Override
+    public String processPayment(double amount) {
+        while(amount <0 ){
+
+            return "Can not process a negative amount!";
+        }
+        return "Card processed with PayPal: "+"card Owner: "+getCardOwner();
+
+    }
+
+    public MobileMoney(String owner, String cardNumber, double amount){
+        super(cardNumber, owner, amount);
+    }
+    @Override
+    public String toString() {
+        return "Owner name: "+getCardOwner()+" || "+"Card Number: "+getCardId() + " || " + "Amount: "+getAmount();
+    }
 }
