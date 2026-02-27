c0b08963624888702b41bf4dc0cf810380d63cf2
manishimwekwizeraj <manishimwekwizeraj@gmail.com>
2026-02-27 19:20:53 +0200

    Created a new package for racticing OOP principles by creating a pyament method package to manage payment by cards using OOP abstraction,polymorphism, encapsulation and inheritence, started off by creating the abstarct class PaymentMethod

--- src/oop/polymorphism/paymentmethods/PaymentMethod.java
+++ src/oop/polymorphism/paymentmethods/PaymentMethod.java
@@ -1,4 +1,43 @@
 package oop.polymorphism.paymentmethods;
 
-public class PaymentMethod {
+public abstract class PaymentMethod {
+    private String cardId;
+    private String cardOwner;
+    private double amount;
+
+    public PaymentMethod(String cardId, String cardOwner, double amount) {
+        this.cardId = cardId;
+        this.cardOwner = cardOwner;
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+
+
+    public String getCardOwner() {
+        return cardOwner;
+    }
+
+    public void setCardOwner(String cardOwner) {
+        this.cardOwner = cardOwner;
+    }
+
+
+    public abstract String processPayment(double amount);
+
 }
