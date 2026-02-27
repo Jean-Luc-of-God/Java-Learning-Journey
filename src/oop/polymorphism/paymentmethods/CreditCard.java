c7cee4f83d076f5935a26c79c154ce0a13ef8faa
manishimwekwizeraj <manishimwekwizeraj@gmail.com>
2026-02-27 19:22:22 +0200

    subclass CreditCrd extending superclass PaymentMethod

--- src/oop/polymorphism/paymentmethods/CreditCard.java
+++ src/oop/polymorphism/paymentmethods/CreditCard.java
@@ -1,4 +1,22 @@
 package oop.polymorphism.paymentmethods;
 
-public class CreditCard {
+public class CreditCard extends PaymentMethod{
+    @Override
+    public String processPayment(double amount) {
+
+        while(amount <0 ){
+
+            return "Can not process a negative amount!";
+        }
+
+        return "Processed payment with Credit Card ";
+    }
+
+    public CreditCard(String owner, String cardNumber, double amount){
+        super(cardNumber, owner, amount);
+    }
+    @Override
+    public String toString() {
+        return "Owner name: "+getCardOwner()+" || "+"Card Number: "+getCardId() + " || " + "Amount: "+getAmount();
+    }
 }
